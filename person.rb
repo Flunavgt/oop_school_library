ad570e823fdab62320293588049a06dbff104b6f
Federico Luna <fluna1973@yahoo.com>
2022-10-31 16:28:32 -0600

    update gem

--- person.rb
+++ person.rb
@@ -2,19 +2,20 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'unknown', parent_permission: true )
-    @id= Random.rand(1..10000)
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?(age)
-    if @age >= 18
-    end
+  private
+
+  def of_age?
+    @age >= 18
+  end
 
   def can_use_services?
     true if is_of_age(@age) == true || parent_permission == true
   end
-
 end
