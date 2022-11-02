8563d5947a3c85bb4f6d923497da113c969ebeb9
Federico Luna <fluna1973@yahoo.com>
2022-11-02 17:33:49 -0600

    placed puts to variables to test

--- first_books.rb
+++ first_books.rb
@@ -8,12 +8,18 @@ booka = Book.new('Erik Story', 'Erik S.')
 bookb = Book.new('Blue whale', 'The fisherman')
 bookc = Book.new('Blue whale returns', 'The fisherman')
 
-rental1 = Rental.new('2022-10-12', booka, fede)
-rental2 = Rental.new('2022-10-12', bookb, may)
-rental3 = Rental.new('2022-08-10', bookc, luis)
-rental4 = Rental.new('2022-08-12', bookb, fede)
+first = Rental.new('2022-10-12', booka, fede)
+second = Rental.new('2022-10-12', bookb, may)
+third = Rental.new('2022-08-10', bookc, luis)
+fourth = Rental.new('2022-08-12', bookb, fede)
 
 puts(fede.rentals.map { |rental| rental.book.title })
 puts(may.rentals.map { |rental| rental.book.title })
 puts(booka.rentals.map { |rental| rental.person.name })
 puts(bookb.rentals.map { |rental| rental.person.name })
+
+# Placed for linters purposes
+puts first
+puts second
+puts third
+puts fourth
