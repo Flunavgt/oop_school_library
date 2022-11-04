2310736e72bfe358dad02a8a33d2c7bf3b770a59
Federico Luna <fluna1973@yahoo.com>
2022-11-04 11:11:52 -0600

    add feature

--- app.rb
+++ app.rb
@@ -69,7 +69,7 @@ class App
     puts 'List of people:'
     puts ''
     @people.each_with_index do |peop, i|
-      puts "#{i + 1}- #{peop.name} age: #{peop.age}"
+      puts "#{i + 1}- #{peop.class} name: #{peop.name} age: #{peop.age}"
     end
     show_link_to_main
   end
